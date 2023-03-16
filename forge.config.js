63989a07cc0e8545ac4c5588eec7de4988eba5ec
Dorian Andrés <dcortesg441@ulacit.ed.cr>
2023-03-15 23:57:31 -0600

    First version

---
+++ forge.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  packagerConfig: {
+    icon: 'icon'
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        options:{
+          setupIcon: 'icon.ico',
+        }
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,68 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 720,
+    height: 480,
+    minHeight: 360,
+    minWidth: 480,
+    autoHideMenuBar: true,
+    transparent: true,
+    frame: false,
+    icon: path.join(__dirname, 'icon.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    }
+  });
+
+  mainWindow.setTitle('Winboard');
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  ipcMain.handle('close', event => BrowserWindow.fromWebContents(event.sender).close())
+  ipcMain.handle('minimize', event => BrowserWindow.fromWebContents(event.sender).minimize())
+  ipcMain.handle('maximize', event => {
+    const win = BrowserWindow.fromWebContents(event.sender)
+    if (win.isMaximized()) win.unmaximize()
+    else win.maximize()
+  })
+  createWindow();
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
